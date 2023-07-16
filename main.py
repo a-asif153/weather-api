962004d61a1653195e22a9240605f86cef9bb3ad
aasif <asifali8121@gmail.com>
2023-07-17 01:19:38 +0500

    some changes + hard coded temp value to test arguments passage

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 
 app = Flask("Website")
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23 #hard coding the value 23 just to test
+    return str(temperature)
 
 
 app.run(debug=True)
